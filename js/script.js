a8569c3cf799dd30fe359cb626d1127c3c0ec58c
maplepancakes <ngsiohongssg@gmail.com>
2021-04-26 21:07:42 +0800

    Addition of constructor items

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+function Book(author, title, pages, read)
+{
+    // constructor
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() 
+{
+
+}
